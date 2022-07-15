2ca9233e5ed517aa398547ce13249bda7c7a8fc3
abhi893 <abhishek98.ac@gmail.com>
2022-07-15 18:59:29 +0530

    completed the app

--- react-todo-app-v1/src/components/TodoList.js
+++ react-todo-app-v1/src/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import TodoForm from './TodoForm'
+import Todo from './Todo';
 
 function TodoList() {
     const [todos, setTodos] = useState([]);
@@ -14,10 +15,37 @@ function TodoList() {
         setTodos(newTodos);
        // console.log(...todos);
     }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id)
+        setTodos(removeArr);
+    }
+
+
+    const updateTodo = (todoId, newValue)=> {
+        if(!newValue.text || /^\s*$/.test(newValue.test)){
+            return;
+        }
+        setTodos(prev => prev.map(item => (item.id === todoId ?newValue : item )))
+    }
+
+
+    const completeTodo = id =>{
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id){
+                todo.isComplete = !todo.isComplete
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
   return (
     <div>
       <h1>What's the plan for Today!!!</h1>
       <TodoForm onSubmit={addTodo}/>
+      <Todo 
+      todos={todos}
+      completeTodo={completeTodo} removeTodo={removeTodo} updateTodo={updateTodo} />
     </div>
   )
 }
